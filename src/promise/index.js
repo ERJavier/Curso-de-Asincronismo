3be4bd0b54fb9db9b7fc9d28e91c40275215ca9d
Javier Cepeda <erjavierc@gmail.com>
2022-06-06 22:39:37 -0400

    promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingwillhappen = () => {
     return new Promise((resolve, reject) =>{
-        if (false) {
+        if (true) {
             resolve('Hey!');
         }else{
             reject('Whoops!');
@@ -11,3 +11,30 @@ const somethingwillhappen = () => {
 somethingwillhappen()
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+
+const somethingwillhappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            setTimeout(() => {
+                resolve('True');
+            }, 2000);
+        } else {
+            const error = new Error('Whoops!');
+            reject(error);
+        }
+    });
+}
+
+somethingwillhappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+Promise.all([somethingwillhappen(), somethingwillhappen2()])
+    .then(response => {
+         console.log('Array of results', response)
+        })
+    .catch(err => {
+        console.error(err);
+    })
